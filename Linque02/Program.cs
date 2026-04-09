c60cab24ad2f8f2abaf93f3d67e38ee115933cf1
ahmedmostafa500 <plmncar500@gmail.com>
2026-04-09 14:27:26 +0200

    question2

--- Linque02/Program.cs
+++ Linque02/Program.cs
@@ -5,9 +5,17 @@
         static void Main(string[] args)
         {
             #region question1
-    //        var top3 = products
-    //.OrderByDescending(p => p.Price)
-    //.Take(3);
+            //        var top3 = products
+            //.OrderByDescending(p => p.Price)
+            //.Take(3);
+            #endregion
+            #region quesion2
+            //int pageNumber = 2;
+            //int pageSize = 5;
+
+            //var result = products
+            //    .Skip((pageNumber - 1) * pageSize)
+            //    .Take(pageSize);
             #endregion
         }
     }
